f00e76e49c14f057abc51b0e8fc8f996684a95c6
Alexandremma <46142888+Alexandremma@users.noreply.github.com>
2019-08-08 19:29:28 -0300

    Implementado Frontend com Vue e consumação da API de Cidades

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <v-app>
+    <div>
+      <v-toolbar dark>
+        <v-toolbar-title>Conta</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+          <v-btn class="link" text router-link to="/cidades">
+            Cidades
+          </v-btn>
+          <v-btn class="link" text router-link to="/agencias">
+            Agências
+          </v-btn>
+          <v-btn class="link" text router-link to="/clientes">
+            Clientes
+          </v-btn>
+        </v-toolbar-items>
+
+      </v-toolbar>
     </div>
-    <router-view/>
-  </div>
+    <router-view></router-view>
+  </v-app>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
 </style>
